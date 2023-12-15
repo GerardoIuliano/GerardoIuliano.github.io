ae3fb46d4a15a28901fb8bd55f5af886f3427847
GerardoIuliano <gerardoiuliano99@gmail.com>
2023-12-15 12:36:28 +0100

    fix hobby and contact

--- utils.js
+++ utils.js
@@ -1,3 +1,10 @@
-function nextImage(){
-    
+function nextImage(idImg, i){
+    const body = document.body
+    srcImg = document.getElementById(idImg).getAttribute("src")
+    end = srcImg.indexOf(".")
+    numImg = srcImg.substring(11,end) // 1.png
+    console.log(numImg);
+    numImg= (numImg % 4) + i
+    console.log(numImg);
+    document.getElementById(idImg).setAttribute("src", "assets/img/"+numImg+".png")
 }
